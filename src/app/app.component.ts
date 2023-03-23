07dc03458b5faebc12d70ee9e3c16441d4cf8759
lucas h <lucas.ph@live.com>
2023-03-23 15:55:06 -0300

    second-challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'second-challenge';
+  title = 'Segundo Desafio - CoderHouse';
+  alumnos: string[] = ['Pepe', 'Moni', 'Paola', 'cocki', 'fatiga'];
+  cargando: boolean = false;
+  esperar: boolean = false;
+  cargarAlumnos() {
+    this.esperar = true;
+    setTimeout(() => {
+      this.esperar = false;
+      this.cargando = true;
+    }, 2000);
+  }
 }
